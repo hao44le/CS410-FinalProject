fae2ea205e0e4c6c2af45921f2ff3710bd0cabc7
Grey Chen <957024640@qq.com>
2019-11-13 21:27:43 +0800

    update the code to include the csv

--- upload_csv_to_elasticsearch.py
+++ upload_csv_to_elasticsearch.py
@@ -1,6 +1,7 @@
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import boto3
+import pandas as pd
 
 host = 'search-chinesek12-sp2avv7lleofzv5qu5m6qkk4by.us-east-2.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
 region = 'us-east-2' # e.g. us-west-1
@@ -8,7 +9,6 @@ region = 'us-east-2' # e.g. us-west-1
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-
 es = Elasticsearch(
     hosts = [{'host': host, 'port': 443}],
     http_auth = awsauth,
@@ -17,12 +17,18 @@ es = Elasticsearch(
     connection_class = RequestsHttpConnection
 )
 
-document = {
-    "title": "Moneyball",
-    "director": "Bennett Miller",
-    "year": "2011"
-}
+csv_name = "K12_chinese_wechat_articles.csv"
 
-es.index(index="movies", doc_type="_doc", id="5", body=document)
+def read_csv(): return pd.read_csv(csv_name)
 
-print(es.get(index="movies", doc_type="_doc", id="5"))
+if __name__ == '__main__':
+    csv = read_csv()
+    # document = {
+    #     "title": "Moneyball",
+    #     "director": "Bennett Miller",
+    #     "year": "2011"
+    # }
+    #
+    # es.index(index="movies", doc_type="_doc", id="5", body=document)
+    #
+    # print(es.get(index="movies", doc_type="_doc", id="5"))
