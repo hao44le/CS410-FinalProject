0a71ef40c857b33393c96dcfc295c44c36660d59
Grey Chen <957024640@qq.com>
2019-11-13 21:20:02 +0800

    add start codebase

---
+++ upload_csv_to_elasticsearch.py
@@ -0,0 +1,28 @@
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import boto3
+
+host = 'search-chinesek12-sp2avv7lleofzv5qu5m6qkk4by.us-east-2.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
+region = 'us-east-2' # e.g. us-west-1
+
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+es = Elasticsearch(
+    hosts = [{'host': host, 'port': 443}],
+    http_auth = awsauth,
+    use_ssl = True,
+    verify_certs = True,
+    connection_class = RequestsHttpConnection
+)
+
+document = {
+    "title": "Moneyball",
+    "director": "Bennett Miller",
+    "year": "2011"
+}
+
+es.index(index="movies", doc_type="_doc", id="5", body=document)
+
+print(es.get(index="movies", doc_type="_doc", id="5"))
