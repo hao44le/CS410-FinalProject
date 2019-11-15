cdc698cdaeec3e273d86cea65b1d89ccedbf68a7
Grey Chen <957024640@qq.com>
2019-11-15 21:08:07 +0800

    update the writer codebase

--- upload_csv_to_elasticsearch.py
+++ upload_csv_to_elasticsearch.py
@@ -1,4 +1,4 @@
-from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch import Elasticsearch, RequestsHttpConnection, helpers
 from requests_aws4auth import AWS4Auth
 import pandas as pd
 import json
@@ -18,16 +18,40 @@ es = Elasticsearch(
     connection_class = RequestsHttpConnection
 )
 #K12_chinese_wechat_articles
-csv_name = "small_test.csv"
+csv_name = "K12_chinese_wechat_articles.csv"
+
+previous_cache = 0
 
 def read_csv_and_write_to_es():
     csv = pd.read_csv(csv_name)
     json_array = json.loads(csv.to_json(orient='index'))
+    actions_array = []
+
     for x in range(0, len(json_array)):
+        if x < previous_cache: continue
+        print("{}/{}".format(x, len(json_array)))
         document = json_array[str(x)]
-        document["发布时间"] = parser.parse(document["发布时间"])
-        es.index(index=index, doc_type="_doc", id=str(x), body=document)
+        try:
+            document["发布时间"] = parser.parse(document["发布时间"])
+        except:
+            continue
+        # es.index(index=index, doc_type="_doc", id=str(x), body=document)
+        document['_id'] = str(x)
+        document['_type'] = "_doc"
+        document["_index"] = index
+        actions_array.append(document)
+
+    try:
+        # make the bulk call, and get a response
+        response = helpers.bulk(es, actions_array, chunk_size=1000, request_timeout=200)
+
+        #response = helpers.bulk(elastic, actions, index='employees', doc_type='people')
+        print ("\nRESPONSE:", response)
+    except Exception as e:
+        print("\nERROR:", e)
 
 if __name__ == '__main__':
-    res = es.search(index=index, body = {'size' : 10000,'query': {'match_all' : {}}})
-    print(res)
+    # read_csv_and_write_to_es()
+    # es.indices.delete(index=index, ignore=[400, 404])
+    # res = es.search(index=index, body = {'size' : 10000,'query': {'match_all' : {}}})
+    # print(res)
