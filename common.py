71a289de95f82f4d8373b56f50cbb55386891ae7
Grey Chen <957024640@qq.com>
2019-11-16 13:26:51 +0800

    add boto3

--- common.py
+++ common.py
@@ -1,12 +1,14 @@
 from elasticsearch import Elasticsearch, RequestsHttpConnection, helpers
 from requests_aws4auth import AWS4Auth
+import boto3
 
 def get_es_instance(index = "chinesek12_wechat_article"):
     host = 'search-chinesek12-sp2avv7lleofzv5qu5m6qkk4by.us-east-2.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
     region = 'us-east-2' # e.g. us-west-1
 
     service = 'es'
-    awsauth = AWS4Auth("AKIATMPP3NOWTKDPP2BX", "+qT9m84DoyWHTf1vs9e6C7exDMEKkU84Dru9O23k", region, service)
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service)
     es = Elasticsearch(
         hosts = [{'host': host, 'port': 443}],
         http_auth = awsauth,
@@ -37,3 +39,7 @@ def es_update_html_content(es, document_id, html_content, index="chinesek12_wech
     # print(doc)
     # es.index(index=index, doc_type="doc",id=document_id,body=doc)
     es.update(index=index, doc_type="_doc", id=document_id, body={"doc": {'文章内容': html_content}})
+
+if __name__ == '__main__':
+    es = get_es_instance()
+    print(es)
